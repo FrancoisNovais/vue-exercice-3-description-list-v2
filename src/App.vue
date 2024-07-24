29e31fbd4f397b5b5ee848af35870a308a866bc8
francois6381 <francois.novais@gmail.com>
2024-07-24 15:07:04 +0200

    Structure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,79 @@
-<script setup></script>
+<script setup>
+import CardDescription from './components/CardDescription.vue'
+
+const personsList = [
+  {
+    gender: 'female',
+    firstname: 'Emily',
+    lastname: 'Parker',
+    age: 24,
+    job: 'teacher',
+    hobbies: ['read books', 'go hiking'],
+    pet: {
+      name: 'Whiskers',
+      age: 2,
+      species: 'ferret'
+    },
+    favoriteColor: 'red'
+  },
+  {
+    gender: 'male',
+    firstname: 'Marcus',
+    lastname: 'Bennett',
+    age: 38,
+    job: 'architect',
+    hobbies: ['watch bird', 'play chess'],
+    pet: {
+      name: 'Nala',
+      age: 6,
+      species: 'dog'
+    },
+    favoriteColor: 'blue'
+  },
+  {
+    gender: 'female',
+    firstname: 'Sophia',
+    lastname: 'Rodriguez',
+    age: 31,
+    job: 'environmental scientist',
+    hobbies: ['play guitar', 'bake cakes'],
+    pet: {
+      name: 'Paws',
+      age: 3,
+      species: 'cat'
+    },
+    favoriteColor: 'orange'
+  },
+  {
+    gender: 'male',
+    firstname: 'Oliver',
+    lastname: 'Hughes',
+    age: 56,
+    job: 'carpenter',
+    hobbies: ['paint', 'ride horses'],
+    pet: {
+      name: 'Bubble',
+      age: 4,
+      species: 'snake'
+    },
+    favoriteColor: 'purple'
+  }
+]
+</script>
 <template>
-  <div>
-    <h1>Hello World</h1>
-  </div>
+  <main>
+    <h1>Description list</h1>
+    <CardDescription />
+  </main>
 </template>
-<style scoped></style>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #c9e1c2;
+  min-height: 100vh;
+  padding: 50px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,16 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/* Import de 'fontawesome core' */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* Import du composant 'vue-fontawesome' */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* Import de l'icône de type 'solid' */
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+
+/* Ajout de l'icône à la bibliothèque de votre projet */
+library.add(faHeart)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
